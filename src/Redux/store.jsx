09e1f57c8e5c00c25acd4df381b2859335769409
Augusto Iphar <87936004+Augustofrx@users.noreply.github.com>
2022-10-02 23:38:55 -0300

    fix: store import

--- src/Redux/store.jsx
+++ src/Redux/store.jsx
@@ -2,7 +2,7 @@ import { applyMiddleware, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 
-import rootReducer from '../reducers/index';
+import rootReducer from '../Redux/Reducer/Reducer'
 
 export const store = createStore(
     rootReducer,
